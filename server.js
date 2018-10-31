6ca9341c82fe890dc04d6ae1ec73d6fbb3a1f945
Cole Phares <cophares@pdx.edu>
2018-10-31 09:35:50 -0700

    Added mongoDb, db model, and api routes

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
+// Database setup
+var dbUrl = 'mongodb://127.0.0.1:27017/memeDb';
+mongoose.connect(dbUrl, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDb connection error'));
+var memes = db.collection('memes');
+
+// Meme model
+var memeSchema = new mongoose.Schema({
+  id: { type: Number, unique: true, index: true, required: true },
+  imagePath: String,
+  text: String,
+  author: String,
+  upvotes: { type: Number, min: 0 },
+  downvotes: { type: Number, min: 0 },
+  created: { type: Date, default: Date.now }
+});
+
+// Compile model from schema
+//var memeModel = mongoose.model('memes', memeSchema);
+
+// Server setup
 const server = express();
 var corsOptions = {
   origin: 'http://localhost:4200',
   optionsSuccessStatus: 200
 }
-
 server.listen(8000, () => {
   console.log('Server started!');
 });
@@ -16,23 +39,51 @@ server.use(cors(corsOptions));
 
 // Get all the memes
 server.route('/api/memes').get((req, res) => {
-  res.send({
-    memes: [{ image: 'lilly', text: 'some text', upvotes: 3892, downvotes: 288 }, { image: 'lucy', text: 'some text', upvotes: 89, downvotes: 28 }]
-  });
+  memes.find().toArray(function(err, result) {
+    if(err) throw err;
+    
+    res.send({
+      result
+    })
+  }) 
 });
 
 // Get all meme images
 server.route('/api/images').get((req, res) => {
-  res.send(200, 'image sent');
+  memes.find().toArray(function(err, result) {
+    if(err) throw err;
+
+    var images = [];
+    for(var r in result) {
+      images.push(result[r].image);
+    }
+    res.send({
+      images
+    })
+  }) 
 });
 
 // Get a specific meme image
-server.route('/api/images/:image').get((req, res) => {
+server.route('/api/images/:id').get((req, res) => {
+  memes.findOne({ "id": req.params.id }, function(err, result) {
+    if(err) throw err;
 
+    var image = result.image;
+    res.send({
+      image
+    })
+  });
+  
 });
 
 // Add a new meme
-server.route('/api/memes').post((req, res) => {
-  res.send(201, req.body);
+server.route('/api/meme').post((req, res) => {
+  memes.insertOne(req.body);
+  memes.findOne({ "imagePath": req.body.imagePath }, function(err, result) {
+    if(err) throw err;
+
+    res.status(200).send(result);
+  })
+  
 });
 
